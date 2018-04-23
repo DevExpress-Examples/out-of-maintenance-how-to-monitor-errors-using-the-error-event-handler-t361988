0ad455bb11e71dea5387866f66d4db760561893e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MapErrorEvent/Form1.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.XtraMap;
+using System;
+using System.Windows.Forms;
+
+namespace MapErrorEvent {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+            ImageLayer imageLayer = new ImageLayer();
+            mapControl1.Layers.Add(imageLayer);
+            WmsDataProvider provider = new WmsDataProvider();
+            imageLayer.DataProvider = provider;
+            provider.ServerUri = "http://YOUR_SERVER_URI";
+            provider.ActiveLayerName = "ACTIVE_LAYER_NAME";
+            imageLayer.Error += OnError;
+        }
+        private void OnError(object sender, MapErrorEventArgs e) {
+            MessageBox.Show(e.Exception.Message);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to monitor errors using the Error event handler
+
+
+This example demonstrates how to monitor errors using the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapLayerBase_Errortopic">LayerBase.Error</a> event handler. For example, this event is raised when an incorrect server URI is specified for a Web Map Service Provider.
+
+<br/>
+
+
